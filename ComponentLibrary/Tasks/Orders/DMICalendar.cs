93611ce6f91cf39078d8bd1e090f7f62bf7fb7bc
Danial Khan <Danialk@calastone.com>
2019-10-24 14:51:07 +0100

    Adding remaining changes

---
+++ ComponentLibrary/Tasks/Orders/DMICalendar.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OpenQA.Selenium;
+using ComponentLibrary.HelperFunctions;
+using ComponentLibrary.Screens;
+
+namespace ComponentLibrary.Actions
+{
+    public class DMICalendar
+    {
+        public static void NavigateToNextMonth(IWebDriver driver)
+        {
+            Find.Element(driver, DMICalendarScreen.ToggleRight).Click();
+        }
+
+        public static void NavigateToPreviousMonth(IWebDriver driver)
+        {
+            Find.Element(driver, DMICalendarScreen.ToggleLeft).Click();
+        }
+    }
+}
